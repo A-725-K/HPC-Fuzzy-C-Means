cc590865ed6cbe0c5b9e25fe2b4d1dedbf90d14a
A-725-K <andrea.canepa.12@gmail.com>
2020-07-23 00:27:34 +0200

    First step in OpenMPI and fixed .gitignore

---
+++ OpenMPI/fuzzy_functions.cpp
@@ -0,0 +1,130 @@
+#include "fuzzy_functions.h"
+
+double distance(point a, point b) {
+    double sum = 0;
+    for(int k=0; k<DIM; k++){
+        sum += pow(a.dims[k] - b.dims[k], 2);
+    }
+    
+    return sqrt(sum);
+}
+
+int bestClusterIndex(double *v) {
+    int idxMax = 0;
+    for (int i=1; i<N_CL; i++)
+        if (v[i] > v[idxMax])
+            idxMax = i;
+    return idxMax;
+}
+
+double fuzzy(point x, point *clusters_centers, int clusterIdx) {
+    double sum = 0.0;
+    for (int i=0; i<N_CL; i++) {
+        double frac = distance(x, clusters_centers[clusterIdx]) / distance(x, clusters_centers[i]);
+        sum += pow(frac, 2/(M-1));
+    }
+    return 1/sum;
+}
+
+void initializeClustersCenters(point *cc, point *X) {
+    for (int i=0; i<N_CL; i++) {
+        int r = (rand() % N_POINTS) + i*N_POINTS;
+        for(int k=0; k<DIM; k++)
+            cc[i].dims[k] = X[r].dims[k];
+    }
+}
+
+void adjustClustersCenters(point *cc, double *mv, point *X, int myid, int P) {
+
+		for (int k=0; k<N_CL; k++) {
+			double sum_dims[DIM];
+			for (int i=0; i<DIM; i++) {
+				for (int j=myid*(SIZE/P); j<(myid+1)*(SIZE/P); j++) {
+					double sum_den = 0.0;
+					double u_ij = pow(mv[j*N_CL+k], M) * X[j].dims[i];
+					
+					MPI_Allreduce(&u_ij, &(sum_dims[i]), 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+					MPI_Allreduce(&u_ij, &sum_den,			 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+				
+					cc[k].dims[i] = sum_dims[i] / sum_den;
+				}
+				
+			}
+		}
+    
+		/*for (int k=0; k<N_CL; k++) {
+        double sum_dims[DIM];
+        for (int i=0; i<DIM; i++)
+            sum_dims[i] = 0.0;
+
+        double sum_den = 0.0;
+        for (int i=0; i<SIZE; i++) {
+            double u_ij_m = pow(mv[i*N_CL + k], M);
+            for(int c=0; c<DIM; c++)
+                sum_dims[c] += u_ij_m * X[i].dims[c];
+            sum_den += u_ij_m;
+        }
+
+        for(int v=0; v<DIM; v++)
+            cc[k].dims[v] = sum_dims[v] / sum_den;
+    }*/
+
+	
+   /* for (int i=0; i<N_CL; i++) {
+        std::cout << "DEBUG: (";
+        for (int j=0; j<DIM; j++)
+            std::cout << cc[i].dims[j] << (j==DIM-1 ? "" : ", ");
+        std::cout << ")" << std::endl;
+    }
+    std::cout << std::endl;*/
+}
+
+void initializeMembershipVecs(double *mv, point *cc, point *X) {
+    for (int i=0; i<SIZE; i++)
+        for (int j=0; j<N_CL; j++)
+            mv[i*N_CL + j] = distance(X[i], cc[j]);
+}
+
+double objectiveFunction(double *mv, point *X, point *cc) {
+    double J = 0.0;
+    for (int i=0; i<SIZE; i++)
+        for (int j=0; j<N_CL; j++)
+            J += pow(mv[i*N_CL + j], M) * pow(distance(X[i], cc[j]), 2);
+    //std::cout << "DEBUG: J = " << J << std::endl;
+    return J;
+}
+
+void fuzzyCMeans(point *X, int *Y, int P, int myid) {
+    double *membership_vecs = new double[SIZE*N_CL];
+    point *clusters_centers = new point[N_CL];
+
+    memset(membership_vecs, 0, sizeof(double)*SIZE*N_CL);
+    initializeClustersCenters(clusters_centers, X);
+    initializeMembershipVecs(membership_vecs, clusters_centers, X);
+
+    int n;
+    double J, old_J;
+    old_J = objectiveFunction(membership_vecs, X, clusters_centers);
+    for (n=0; n<N_ITER; n++) {
+        adjustClustersCenters(clusters_centers, membership_vecs, X, myid, P);
+        for (int i=myid*(SIZE/P); i<(myid+1)*(n/P); i++)
+            for (int j=0; j<N_CL; j++)
+                membership_vecs[i*N_CL + j] = fuzzy(X[i], clusters_centers, j);
+    
+        
+        J = objectiveFunction(membership_vecs, X, clusters_centers);
+        if (fabs(old_J - J) < EPS)
+            break;
+        /*else
+            std::cout << J << std::endl;*/
+        old_J = J;
+    }
+
+    for (int i=0; i<SIZE; i++)
+        Y[i] = bestClusterIndex(membership_vecs + i*N_CL);
+    
+    //std::cout << n << std::endl;
+    delete[] membership_vecs;
+    delete[] clusters_centers;
+}
+
---
+++ OpenMPI/fuzzy_functions.h
@@ -0,0 +1,11 @@
+#include "../common/common.h"
+#include <mpi.h>
+
+double distance(point, point);
+int bestClusterIndex(double*);
+double fuzzy(point, point*, int);
+void initializeClustersCenters(point*, point*);
+void initializeMembershipVecs(double*, point*, point*);
+void adjustClustersCenters(point*, double*, point*, int, int);
+double objectiveFunction(double*, point*, point*);
+void fuzzyCMeans(point*, int*, int, int);
