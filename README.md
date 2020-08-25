3c9e276923ec93f980f8424f22c4b277b5fec277
A-725-K <andrea.canepa.12@gmail.com>
2020-08-25 18:15:19 +0200

    Updated README.md and uncommented tests in do_tests.sh

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HPC-Fuzzy-C-Means
 A survey on performance of Fuzzy C-Means implemented in a parallel and distributed way.
+
+***High Performance Computing*** final project
+
+## Authors
+* **<i>Andrea Canepa</i>** - Computer Science, UNIGE a.y. 2019/2020 - *S 4185248*
+* **<i>Francesco Pellaco</i>** - Computer Science, UNIGE a.y. 2019/2020 - *S 4228743*
--- do_tests.sh
+++ do_tests.sh
@@ -46,79 +46,75 @@ function print_title() {
 ### MAIN ###
 ############
 
-#print_title SEQUENTIAL
-#
-## sequential
-#SEQ_BIN="$BIN_DIR/$SEQ/${FZY}_"
-#SEQ_OUT="$RES_DIR/$SEQ/${TIME}_"
-#for opt in $OPT; do
-#	echo -n "Testing: $SEQ_BIN$opt"
-#	for i in `seq 1 $N_REP`; do
-#		./"$SEQ_BIN$opt" >> "$SEQ_OUT$opt"
-#		echo -n '.'
-#	done
-#	echo ''
-#done
-#
-#print_title PARALLEL
-#
-## parallel - OpenMP
-#PAR_BIN="$BIN_DIR/$PAR/"
-#PAR_OUT="$RES_DIR/$PAR/${TIME}_"
-#for sched in $SCHED; do
-#	for opt in $OPT; do
-#		echo -n "Testing: $PAR_BIN/$sched/${FZY}_${sched}_$opt"
-#		for i in `seq 1 $N_REP`; do
-#			./"$PAR_BIN/$sched/${FZY}_${sched}_$opt" >> "$PAR_OUT${sched}_$opt"
-#			echo -n '.'
-#		done
-#		echo ''
-#	done
-#done
-#
-#
-#print_title MPI SCALING
-#
-## Weak VS Strong MPI scaling
-#MPIS_BIN="$BIN_DIR/$MPI/${FZY}_${MPI}_scaling"
-#MPI_OUT="$RES_DIR/$MPI/${TIME}_scaling_"
-#
-#
-#
+print_title SEQUENTIAL
+
+# sequential
+SEQ_BIN="$BIN_DIR/$SEQ/${FZY}_"
+SEQ_OUT="$RES_DIR/$SEQ/${TIME}_"
+for opt in $OPT; do
+	echo -n "Testing: $SEQ_BIN$opt"
+	for i in `seq 1 $N_REP`; do
+		./"$SEQ_BIN$opt" >> "$SEQ_OUT$opt"
+		echo -n '.'
+	done
+	echo ''
+done
+
+
+print_title PARALLEL
+
+# parallel - OpenMP
+PAR_BIN="$BIN_DIR/$PAR/"
+PAR_OUT="$RES_DIR/$PAR/${TIME}_"
+for sched in $SCHED; do
+	for opt in $OPT; do
+		echo -n "Testing: $PAR_BIN/$sched/${FZY}_${sched}_$opt"
+		for i in `seq 1 $N_REP`; do
+			./"$PAR_BIN/$sched/${FZY}_${sched}_$opt" >> "$PAR_OUT${sched}_$opt"
+			echo -n '.'
+		done
+		echo ''
+	done
+done
+
+
+print_title MPI SCALING
+
+# Weak VS Strong MPI scaling
+SCALING_DIR=common/dataset/scaling
+MPI_BIN_SC="$BIN_DIR/$MPI/${FZY}_${MPI}_scaling"
+MPI_SSC_RES="$RES_DIR/$MPI/strong_scaling"
+MPI_WSC_RES="$RES_DIR/$MPI/weak_scaling"
+
+# STRONG
+echo 'Testing STRONG SCALING:'
+for np in `seq 2 20`; do
+	echo -n "Num processors: $np"
+	echo $np >> $MPI_SSC_RES
+	for i in `seq 1 $NREP_SCALING`; do
+		mpirun -n $np -nolocal --hostfile OpenMPI/host_file $MPI_BIN_SC $SCALING_DIR/STRONG.csv >> $MPI_SSC_RES
+		echo -n '.'
+	done
+	echo ''
+done
+echo ''
+
+# WEAK
+echo 'Testing WEAK SCALING (1000 points/process):'
+for np in `seq 10 20`; do
+	SIZE=$(( np * 1000 ))
+	echo -ne "Num processors: $np\tDataset size: $SIZE"
+	echo "$np $SIZE" >> $MPI_WSC_RES
+	for i in `seq 1 $NREP_SCALING`; do
+		mpirun -n $np -nolocal --hostfile OpenMPI/host_file $MPI_BIN_SC $SCALING_DIR/WEAK_$SIZE.csv >> $MPI_WSC_RES
+		echo -n '.'
+	done
+	echo ''
+done
+
+
 print_title MPI
-#
-# OpenMPI scaling
-#SCALING_DIR=common/dataset/scaling
-#MPI_BIN_SC="$BIN_DIR/$MPI/${FZY}_${MPI}_scaling"
-#MPI_SSC_RES="$RES_DIR/$MPI/strong_scaling"
-#MPI_WSC_RES="$RES_DIR/$MPI/weak_scaling"
-#
-## STRONG
-#echo 'Testing STRONG SCALING:'
-#for np in `seq 2 20`; do
-#	echo -n "Num processors: $np"
-#	echo $np >> $MPI_SSC_RES
-#	for i in `seq 1 $NREP_SCALING`; do
-#		mpirun -n $np -nolocal --hostfile OpenMPI/host_file $MPI_BIN_SC $SCALING_DIR/STRONG.csv >> $MPI_SSC_RES
-#		echo -n '.'
-#	done
-#	echo ''
-#done
-#echo ''
-#
-## WEAK
-#echo 'Testing WEAK SCALING (1000 points/process):'
-#for np in `seq 10 20`; do
-#	SIZE=$(( np * 1000 ))
-#	echo -ne "Num processors: $np\tDataset size: $SIZE"
-#	echo "$np $SIZE" >> $MPI_WSC_RES
-#	for i in `seq 1 $NREP_SCALING`; do
-#		mpirun -n $np -nolocal --hostfile OpenMPI/host_file $MPI_BIN_SC $SCALING_DIR/WEAK_$SIZE.csv >> $MPI_WSC_RES
-#		echo -n '.'
-#	done
-#	echo ''
-#done
-#
+
 # MPI (best np)
 MPI_BIN="$BIN_DIR/$MPI/${FZY}_${MPI}_"
 MPI_OUT="$RES_DIR/$MPI/${TIME}_${MPI}_"
